658fd7fab666922d82aca8ef1ce47e13519d965d
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2020-12-09 18:05:22 +0100

    Add files via upload

---
+++ data/MAISON_4.js
@@ -0,0 +1,10 @@
+var json_MAISON_4 = {
+"type": "FeatureCollection",
+"name": "MAISON_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Maison": "Nous" }, "geometry": { "type": "Point", "coordinates": [ -0.700651673821532, 44.79099232863561 ] } },
+{ "type": "Feature", "properties": { "id": null, "Maison": "Travail MAMAN" }, "geometry": { "type": "Point", "coordinates": [ -0.713829786571337, 44.765431900360142 ] } },
+{ "type": "Feature", "properties": { "id": null, "Maison": "Travail PAPA" }, "geometry": { "type": "Point", "coordinates": [ -0.77101069065373, 44.736246195154813 ] } }
+]
+}
