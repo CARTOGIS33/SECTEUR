bb831018f71ab38c84dd3cb403d5b82892987e00
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2020-12-22 09:36:17 +0100

    Add files via upload

--- data/MAISON_5.js
+++ data/MAISON_5.js
@@ -5,6 +5,7 @@ var json_MAISON_5 = {
 "features": [
 { "type": "Feature", "properties": { "id": null, "Maison": "Nous" }, "geometry": { "type": "Point", "coordinates": [ -0.700651673821532, 44.79099232863561 ] } },
 { "type": "Feature", "properties": { "id": null, "Maison": "Travail MAMAN" }, "geometry": { "type": "Point", "coordinates": [ -0.713829786571337, 44.765431900360142 ] } },
-{ "type": "Feature", "properties": { "id": null, "Maison": "Travail PAPA" }, "geometry": { "type": "Point", "coordinates": [ -0.77101069065373, 44.736246195154813 ] } }
+{ "type": "Feature", "properties": { "id": null, "Maison": "Travail PAPA" }, "geometry": { "type": "Point", "coordinates": [ -0.77101069065373, 44.736246195154813 ] } },
+{ "type": "Feature", "properties": { "id": null, "Maison": "Kévin" }, "geometry": { "type": "Point", "coordinates": [ -0.649973956880426, 44.817993738055392 ] } }
 ]
 }
